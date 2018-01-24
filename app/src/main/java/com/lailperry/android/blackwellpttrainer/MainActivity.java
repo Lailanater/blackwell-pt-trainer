6b243003eeda3e7f9e07db9a10c3205049652a5f
Chris Lail <chrislail56@gmail.com>
2018-01-24 17:14:50 -0500

    Added blank Toasts for simple actions

--- app/src/main/java/com/lailperry/android/blackwellpttrainer/MainActivity.java
+++ app/src/main/java/com/lailperry/android/blackwellpttrainer/MainActivity.java
@@ -2,7 +2,8 @@ package com.lailperry.android.blackwellpttrainer;
 
 import android.content.Intent;
 import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +15,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -27,14 +30,21 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /* FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(MainActivity.this, "You clicked on Today's Workout!", Toast.LENGTH_SHORT).show();
             }
         });
-        */
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                Toast.makeText(MainActivity.this, "You touched the workout for " + (++month) + "-" + day + "-" + year, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -94,13 +104,13 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_workouts) {
-
+            Toast.makeText(this.getApplicationContext(), "You clicked on Workouts!", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_stats) {
-
+            Toast.makeText(this.getApplicationContext(), "You clicked on Stats!", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_groups) {
-
+            Toast.makeText(this.getApplicationContext(), "You clicked on Groups!", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_tips) {
-
+            Toast.makeText(this.getApplicationContext(), "You clicked on Tips!", Toast.LENGTH_SHORT).show();
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
