aff0fb096c92704515a1cbe7351827fd9cab03cf
Chris Lail <chrislail56@gmail.com>
2018-02-21 17:17:58 -0500

    Preparing to add a WorkoutDatabase in SQLite to save information

---
+++ app/src/main/java/com/lailperry/android/blackwellpttrainer/WorkoutDatabase.java
@@ -0,0 +1,32 @@
+package com.lailperry.android.blackwellpttrainer;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Owner on 2/21/2018.
+ */
+
+public class WorkoutDatabase extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "workouts.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_WORKOUTS = "workouts";
+    private static final String FIELD_NAME = "name";
+    private static final String FIELD_DESCRIPTION = "description";
+
+    public WorkoutDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Upgrade code goes here
+    }
+}
