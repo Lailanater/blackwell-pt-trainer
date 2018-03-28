e206302e4c094dcc6ed6626e00bf0101ac1d002b
Chris Lail <chrislail56@gmail.com>
2018-03-28 17:40:10 -0400

    Added Day and Night modes

--- app/src/main/java/com/lailperry/android/blackwellpttrainer/MainActivity.java
+++ app/src/main/java/com/lailperry/android/blackwellpttrainer/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,6 +34,12 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+            recreate();
+        }
 
         mDB = new WorkoutDatabase(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
--- app/src/main/java/com/lailperry/android/blackwellpttrainer/SettingsFragment.java
+++ app/src/main/java/com/lailperry/android/blackwellpttrainer/SettingsFragment.java
@@ -2,15 +2,19 @@ package com.lailperry.android.blackwellpttrainer;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 /**
  * Created by Owner on 2/20/2018.
  */
 
 public class SettingsFragment extends Fragment {
+
+    public CheckBox mThemeCheckBox;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -22,6 +26,21 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        mThemeCheckBox = v.findViewById(R.id.themeCheckbox);
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO)
+            mThemeCheckBox.setChecked(true);
+        mThemeCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = mThemeCheckBox.isChecked();
+                if (checked)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                else
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                getActivity().recreate();
+            }
+        });
 
         return v;
     }
