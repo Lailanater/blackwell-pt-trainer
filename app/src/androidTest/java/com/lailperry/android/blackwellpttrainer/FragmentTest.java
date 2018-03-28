e506ce99d06f4e9eb6d573e8598090b1328efe46
Chris Lail <chrislail56@gmail.com>
2018-03-28 15:55:18 -0400

    Fixed automated testing

--- app/src/androidTest/java/com/lailperry/android/blackwellpttrainer/FragmentTest.java
+++ app/src/androidTest/java/com/lailperry/android/blackwellpttrainer/FragmentTest.java
@@ -2,6 +2,7 @@ package com.lailperry.android.blackwellpttrainer;
 
 
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -12,6 +13,7 @@ import android.view.ViewParent;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,14 +21,17 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withResourceName;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -35,27 +40,37 @@ public class FragmentTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
     @Test
     public void fragmentTest() {
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab),
+                        childAtPosition(
+                                allOf(withId(R.id.fragment_container),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        onView(withId(R.id.workoutsDetailLinearLayout))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.beginWorkoutButton))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.fab))
+                .check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.beginWorkoutButton))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        pressBack();
+
+        onView(withId(R.id.workouts_recycler_view))
+                .check(matches(isDisplayed()));
+
         ViewInteraction appCompatImageButton = onView(
                 allOf(withContentDescription("Open navigation drawer"),
                         childAtPosition(
@@ -73,11 +88,14 @@ public class FragmentTest {
                                 childAtPosition(
                                         withId(R.id.nav_view),
                                         0)),
-                        1),
+                        2),
                         isDisplayed()));
         navigationMenuItemView.perform(click());
 
-        onView(withId(R.id.workouts_recycler_view))
+        onView(withId(R.id.statsFrameLayout))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.fab))
                 .check(matches(isDisplayed()));
 
         ViewInteraction appCompatImageButton2 = onView(
@@ -91,18 +109,20 @@ public class FragmentTest {
                         isDisplayed()));
         appCompatImageButton2.perform(click());
 
-
         ViewInteraction navigationMenuItemView2 = onView(
                 allOf(childAtPosition(
                         allOf(withId(R.id.design_navigation_view),
                                 childAtPosition(
                                         withId(R.id.nav_view),
                                         0)),
-                        2),
+                        3),
                         isDisplayed()));
         navigationMenuItemView2.perform(click());
 
-        onView(withId(R.id.statsFrameLayout))
+        onView(withId(R.id.groupsLayout))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.fab))
                 .check(matches(isDisplayed()));
 
         ViewInteraction appCompatImageButton3 = onView(
@@ -122,11 +142,14 @@ public class FragmentTest {
                                 childAtPosition(
                                         withId(R.id.nav_view),
                                         0)),
-                        3),
+                        4),
                         isDisplayed()));
         navigationMenuItemView3.perform(click());
 
-        onView(withId(R.id.groupsLayout))
+        onView(withId(R.id.tipsFrameLayout))
+                .check(matches(isDisplayed()));
+
+        onView(withId(R.id.fab))
                 .check(matches(isDisplayed()));
 
         ViewInteraction appCompatImageButton4 = onView(
@@ -146,67 +169,42 @@ public class FragmentTest {
                                 childAtPosition(
                                         withId(R.id.nav_view),
                                         0)),
-                        4),
-                        isDisplayed()));
-        navigationMenuItemView4.perform(click());
-
-        ViewInteraction appCompatImageButton5 = onView(
-                allOf(withContentDescription("Open navigation drawer"),
-                        childAtPosition(
-                                allOf(withId(R.id.toolbar),
-                                        childAtPosition(
-                                                withClassName(is("android.support.design.widget.AppBarLayout")),
-                                                0)),
-                                1),
-                        isDisplayed()));
-        appCompatImageButton5.perform(click());
-
-        onView(withId(R.id.tipsFrameLayout))
-                .check(matches(isDisplayed()));
-
-        ViewInteraction navigationMenuItemView5 = onView(
-                allOf(childAtPosition(
-                        allOf(withId(R.id.design_navigation_view),
-                                childAtPosition(
-                                        withId(R.id.nav_view),
-                                        0)),
                         5),
                         isDisplayed()));
-        navigationMenuItemView5.perform(click());
+        navigationMenuItemView4.perform(click());
 
         onView(withId(R.id.settingsLayout))
                 .check(matches(isDisplayed()));
 
-        pressBack();
-
-        ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.fab),
-                        childAtPosition(
-                                allOf(withId(R.id.fragment_container),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                1)),
-                                0),
-                        isDisplayed()));
-        floatingActionButton.perform(click());
-
-        onView(withId(R.id.workoutsDetailLinearLayout))
+        onView(withId(R.id.fab))
                 .check(matches(isDisplayed()));
-
-        ViewInteraction appCompatButton = onView(
-                allOf(withId(R.id.readyButton), withText("Ready"),
-                        childAtPosition(
-                                allOf(withId(R.id.workoutsDetailLinearLayout),
-                                        childAtPosition(
-                                                withId(R.id.fragment_container),
-                                                1)),
-                                3),
-                        isDisplayed()));
-        appCompatButton.perform(click());
 
         pressBack();
 
         onView(withId(R.id.workouts_recycler_view))
                 .check(matches(isDisplayed()));
+
+        onView(withId(R.id.fab))
+                .check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
