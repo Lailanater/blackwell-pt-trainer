f4ff34fa719dacd91a3f66360a38d37051490044
Chris Lail <chrislail56@gmail.com>
2018-04-19 18:43:30 -0400

    Refactored WorkoutsDetailFragment

--- app/src/main/java/com/lailperry/android/blackwellpttrainer/WorkoutsDetailFragment.java
+++ app/src/main/java/com/lailperry/android/blackwellpttrainer/WorkoutsDetailFragment.java
@@ -33,6 +33,9 @@ public class WorkoutsDetailFragment extends Fragment {
     private Button mStartTimerButton;
     private Button mBeginRunButton;
     private Workout mWorkout;
+    private SharedPreferences mSharedPreferences;
+    private ArrayList<CheckBox> mAllCheckboxes = new ArrayList<>();
+    private LinearLayout mDividerLine;
 
     @SuppressLint("ValidFragment")
     public WorkoutsDetailFragment(Workout workout) {
@@ -46,61 +49,18 @@ public class WorkoutsDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_workouts_detail, container, false);
 
-        mLinearLayout = v.findViewById(R.id.workoutDetailContentLinearLayout);
-        mWorkoutName = v.findViewById(R.id.workoutNameTextView);
-        mWorkoutName.setText(mWorkout.getName());
-        mWorkoutDescription = v.findViewById(R.id.workout_detail_description);
-        mWorkoutDescription.setText(mWorkout.getDescription());
-        int size = mWorkout.getContent().size();
-        final ArrayList<CheckBox> allCheckboxes = new ArrayList<>();
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-        mWorkout.setComplete(preferences.getBoolean(mWorkout.getName(), false));
-        for (int i = 0; i < size; i++) {
-            CheckBox checkBox = new CheckBox(getContext());
-            checkBox.setText(mWorkout.getContent().get(i));
-            checkBox.setPadding(0, 75, 0, 75);
-            boolean checked = preferences.getBoolean(mWorkout.getName() + i, false);
-            checkBox.setChecked(checked);
-            allCheckboxes.add(checkBox);
-            mLinearLayout.addView(checkBox);
-            LinearLayout line = new LinearLayout(getContext());
-            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
-                line.setBackgroundColor(getResources().getColor(R.color.cardview_dark_background));
-            } else {
-                line.setBackgroundColor(getResources().getColor(R.color.cardview_light_background));
-            }
-            line.setMinimumHeight(2);
-            line.setOrientation(LinearLayout.VERTICAL);
-            if (i != size - 1)
-                mLinearLayout.addView(line);
-        }
-        for (int i = 0; i < allCheckboxes.size(); i++) {
-            final CheckBox checkBox = allCheckboxes.get(i);
-            final int finalI = i;
-            checkBox.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    boolean tasksCompleted = allTasksCompleted(allCheckboxes);
-                    mWorkout.setComplete(tasksCompleted);
-                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
-                    SharedPreferences.Editor edit = preferences.edit();
-                    edit.putBoolean(mWorkout.getName() + finalI, checkBox.isChecked());
-                    edit.putBoolean(mWorkout.getName(), tasksCompleted);
-                    edit.apply();
-                }
-            });
-        }
-        mBeginRunButton = v.findViewById(R.id.beginRunButton);
-        mBeginRunButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Fitness.ACTION_TRACK);
-                intent.setType("vnd.google.fitness.activity/running");
-                intent.putExtra("actionStatus", "ActiveActionStatus");
-                startActivity(intent);
-            }
-        });
-        mStartTimerButton = v.findViewById(R.id.startTimerButton);
+        initializeViews(v);
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        setWorkoutCompleteFromPreferences();
+        dynamicallyCreateCheckboxes();
+        setOnClickListenersToAllCheckboxes();
+        setOnClickListenerForBeginRunButton();
+        setOnClickListenerForStartTimerButton();
+
+        return v;
+    }
+
+    private void setOnClickListenerForStartTimerButton() {
         mStartTimerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -108,8 +68,65 @@ public class WorkoutsDetailFragment extends Fragment {
                 startActivity(intent);
             }
         });
+    }
 
-        return v;
+    private void setOnClickListenerForBeginRunButton() {
+        mBeginRunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = createFitnessIntent();
+                startActivity(intent);
+            }
+        });
+    }
+
+    public Intent createFitnessIntent() {
+        Intent intent = new Intent(Fitness.ACTION_TRACK);
+        intent.setType("vnd.google.fitness.activity/running");
+        intent.putExtra("actionStatus", "ActiveActionStatus");
+        return intent;
+    }
+
+    private void setOnClickListenersToAllCheckboxes() {
+        for (final CheckBox checkBox : mAllCheckboxes) {
+            checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean tasksCompleted = allTasksCompleted(mAllCheckboxes);
+                    mWorkout.setComplete(tasksCompleted);
+                    saveIsCheckedToPreferences(checkBox);
+                    saveIsEntireWorkoutCompleteToPreferences(tasksCompleted);
+                }
+            });
+        }
+    }
+
+    private void saveIsEntireWorkoutCompleteToPreferences(boolean tasksCompleted) {
+        SharedPreferences.Editor edit = mSharedPreferences.edit();
+        edit.putBoolean(mWorkout.getName(), tasksCompleted);
+        edit.apply();
+    }
+
+    private void saveIsCheckedToPreferences(CheckBox checkBox) {
+        SharedPreferences.Editor edit = mSharedPreferences.edit();
+        edit.putBoolean(mWorkout.getName() + mAllCheckboxes.indexOf(checkBox), checkBox.isChecked());
+        edit.apply();
+    }
+
+    private void dynamicallyCreateCheckboxes() {
+        int last = getContentSizeFromWorkout() - 1;
+        for (int i = 0; i < getContentSizeFromWorkout(); i++) {
+            CheckBox checkBox = createFormattedCheckbox(i);
+            checkBox.setChecked(isCheckboxChecked(i));
+            addCheckboxToLayout(checkBox);
+            createNewDividerLine();
+            if (i != last)
+                addDividerLineToView();
+        }
+    }
+
+    private void setWorkoutCompleteFromPreferences() {
+        mWorkout.setComplete(mSharedPreferences.getBoolean(mWorkout.getName(), false));
     }
 
     private boolean allTasksCompleted(ArrayList<CheckBox> allCheckboxes) {
@@ -124,4 +141,48 @@ public class WorkoutsDetailFragment extends Fragment {
         return flag;
     }
 
+    public void initializeViews(View v) {
+        mLinearLayout = v.findViewById(R.id.workoutDetailContentLinearLayout);
+        mWorkoutName = v.findViewById(R.id.workoutNameTextView);
+        mWorkoutName.setText(mWorkout.getName());
+        mWorkoutDescription = v.findViewById(R.id.workout_detail_description);
+        mWorkoutDescription.setText(mWorkout.getDescription());
+        mBeginRunButton = v.findViewById(R.id.beginRunButton);
+        mStartTimerButton = v.findViewById(R.id.startTimerButton);
+    }
+
+    private int getContentSizeFromWorkout() {
+        return mWorkout.getContent().size();
+    }
+
+    private CheckBox createFormattedCheckbox(int location) {
+        CheckBox checkBox = new CheckBox(getContext());
+        checkBox.setText(mWorkout.getContent().get(location));
+        checkBox.setPadding(0, 75, 0, 75);
+        return checkBox;
+    }
+
+    private boolean isCheckboxChecked(int location) {
+        return mSharedPreferences.getBoolean(mWorkout.getName() + location, false);
+    }
+
+    private void addCheckboxToLayout(CheckBox checkBox) {
+        mAllCheckboxes.add(checkBox);
+        mLinearLayout.addView(checkBox);
+    }
+
+    private void createNewDividerLine() {
+        mDividerLine = new LinearLayout(getContext());
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO) {
+            mDividerLine.setBackgroundColor(getResources().getColor(R.color.cardview_dark_background));
+        } else {
+            mDividerLine.setBackgroundColor(getResources().getColor(R.color.cardview_light_background));
+        }
+        mDividerLine.setMinimumHeight(2);
+        mDividerLine.setOrientation(LinearLayout.VERTICAL);
+    }
+
+    private void addDividerLineToView() {
+        mLinearLayout.addView(mDividerLine);
+    }
 }
